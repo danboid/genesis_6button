386719fbf61c6c2287ca75fb0b7c1658fbcb94d6
Dan MacDonald <allcoms@gmail.com>
2025-10-04 20:06:47 +0100

    Use -O3 option for Linux build script to match VisualC project build options

--- atmel_studio/genesis_6button/build.sh
+++ atmel_studio/genesis_6button/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # You need avr-gcc v8 or later and avr-libc to build for the attiny404
  
-avr-gcc -mmcu=attiny404 -Os -Wall -Wextra main.c -o main.elf
+avr-gcc -mmcu=attiny404 -O3 -Wall -Wextra main.c -o main.elf
 avr-objcopy -O ihex -R .eeprom main.elf main.hex
