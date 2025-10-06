07a9dfef6a0a923b32ff02551008e7f85ecb8ce6
Dan MacDonald <allcoms@gmail.com>
2025-10-06 10:53:17 +0100

    Update build script to remove old build files before building

--- atmel_studio/genesis_6button/build.sh
+++ atmel_studio/genesis_6button/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# You need avr-gcc v8 or later and avr-libc to build for the attiny404
- 
+# This is a script to build the 6 button Mega Drive controller PCB firmware.
+# You need avr-gcc version 8 or later and avr-libc installed to build this code.
+
+# Remove previous build, if it exists.
+rm -f main.elf main.hex
+
+# Build controller firmware.
 avr-gcc -mmcu=attiny404 -O3 -Wall -Wextra main.c -o main.elf
 avr-objcopy -O ihex -R .eeprom main.elf main.hex
