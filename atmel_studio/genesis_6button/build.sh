962a2420da887331e30169b63c3124555128c8aa
Dan MacDonald <allcoms@gmail.com>
2026-02-03 19:50:41 +0000

    Tweak build script

--- atmel_studio/genesis_6button/build.sh
+++ atmel_studio/genesis_6button/build.sh
@@ -2,6 +2,12 @@
 # This is a script to build the 6 button Mega Drive controller PCB firmware.
 # You need avr-gcc version 8 or later and avr-libc installed to build this code.
 
+# Exit on error, unset variables, catch errors behind a pipe
+set -euo pipefail
+
+# Move to the directory where the script is located
+cd "$(dirname "$0")"
+
 # Remove previous build, if it exists.
 rm -f main.elf main.hex
 
